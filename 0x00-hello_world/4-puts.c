35672e8a900a8f6e4864b62f8ecf726aeb81145f
harrysink <harrisonfemi@yahoo.com>
2022-10-13 21:30:44 -0700

    c prints text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - should print "Programming is like building a multilingual puzzle"
+ *return - should print 0 if successsful, if not, print non-zero
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
