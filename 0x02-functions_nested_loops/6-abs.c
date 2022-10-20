5cc498465bb84ee335f555e48bbfc6a1108ae1ff
harrysink <harrisonfemi@yahoo.com>
2022-10-20 04:19:52 +0000

    abs int val compute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute integer value
+ * @n: int for check
+ * Return: absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
