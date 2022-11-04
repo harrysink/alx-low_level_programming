09e6dfe8b9748a08038c89ca0713b8f8aaef7de6
harrysink <harrisonfemi@yahoo.com>
2022-11-04 19:10:21 +0000

    task 4 commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * rev_string - this function reverses a string
+ @s: string to reverse
+ */
+void rev_string(char *s)
+{
+
---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * puts2 - this function prints every other character of a
+ * string, starting the first character, followed by a new line
+ * @str: string to be printed
+ */
+void puts2(char *str)
+{
+	char str;
+
+
